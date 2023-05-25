62d44429e8227e322c732ceacb72b5516232d0cb
Davranbek <davranbek_1995@mail.ru>
2023-05-25 19:51:52 +0500

    added trafico maket

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.clients__slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: false,  // бесконечный цикл
+    slidesToShow: 2.2,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+});
+
